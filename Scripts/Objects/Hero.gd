880c290eb0a5291aac1fc851d2331ef42a1d120d
Blinc <65546512+Blinc13@users.noreply.github.com>
2021-11-25 23:30:02 +0300

    Update

--- Scripts/Objects/Hero.gd
+++ Scripts/Objects/Hero.gd
@@ -5,28 +5,37 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
-var velocity: Vector2
 const speed=200
+const relTime=0.1
+var velocity: Vector2
+var reload=relTime
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func _input(event):
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("Up"):
-		velocity.y-=speed
+		velocity.y-=1
 	if Input.is_action_pressed("Down"):
-		velocity.y+=speed
+		velocity.y+=1
 	if Input.is_action_pressed("Left"):
-		velocity.x-=speed
+		velocity.x-=1
 		$AnimatedSprite.flip_h=true
 	if Input.is_action_pressed("Right"):
-		velocity.x+=speed
+		velocity.x+=1
 		$AnimatedSprite.flip_h=false
 	
-	move_and_slide(velocity)
-	velocity=Vector2()
+	if Input.is_action_pressed("Fire") and reload>=relTime:
+		reload=0
+		print_debug("А ну заткнись, этого нет, бюджет-1 пачка дошика")
 	
-	print_debug(delta)
+	move_and_slide(velocity.normalized()*speed)
+	velocity=Vector2()
+	reload+=delta
+	
+	#print_debug(delta)
