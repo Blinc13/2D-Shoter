0e2fc9cfeb01e5f79aa60ac02148f75482dc1064
LeMoN-Code-Creators <55497273+LeMoN-Code-Creators@users.noreply.github.com>
2021-12-04 13:14:48 +0200

    add btns

--- Scripts/Load/MainMenu.gd
+++ Scripts/Load/MainMenu.gd
@@ -1,6 +1,11 @@
 extends Control
-
-func _on_Button_pressed():
+func _on_PlayBtn_pressed():
 	visible=false
 	get_tree().root.add_child(load("res://Scenes/Levels/BetaLevel.tscn").instance())
 	print(SIZE_EXPAND)
+
+func _on_SettingsBtn_pressed():
+	$TabContainer.visible = !$TabContainer.visible
+
+func _on_ExitBtn_pressed():
+	pass # Replace with function body.
