3730a7009c296cfa3f57f48b07bb6f5d55bcf514
Blinc <65546512+Blinc13@users.noreply.github.com>
2021-12-05 02:00:43 +0300

    Edited base weapon class

--- Scripts/Objects/Weapons/Weapon.gd
+++ Scripts/Objects/Weapons/Weapon.gd
@@ -2,13 +2,19 @@ extends AnimatedSprite
 class_name Weapon
 
 export(float) var relTime
-
+export(float) var BulletBoost
 export(PackedScene) var Bullet
+
+var reload:float
 
 func _init():
 	position=Vector2(0,10)
 
+func _process(delta):
+	reload+=delta
+
 func fire(vel:Vector2,pos:Vector2):
 	var node=Bullet.instance()
-	node.start(vel,pos)
+	reload=0
+	node.start(vel,pos,BulletBoost)
 	get_node("/root/Level").add_child(node)
